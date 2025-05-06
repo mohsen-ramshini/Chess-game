72ee9869f595dc125f0b27843586c6e3ec86a6e8
mohsen-ramshini <rsmohsen20@gmail.com>
2025-05-06 23:38:49 +0330

    chess board created

---
+++ scripts/config/constants.js
@@ -0,0 +1,8 @@
+export const Pieces = {
+    "king": {w: "./assets/styles/images/pieces/white-king.html",b:"./assets/styles/images/pieces/black-king.html",index:["d"]},
+    "queen": {w: "./assets/styles/images/pieces/white-queen.html",b:"./assets/styles/images/pieces/black-queen.html",index:["e"]},
+    "rook": {w: "./assets/styles/images/pieces/white-rook.html",b:"./assets/styles/images/pieces/black-rook.html",index:["a","h"]},
+    "knight": {w: "./assets/styles/images/pieces/white-knight.html",b:"./assets/styles/images/pieces/black-knight.html",index:["b","g"]},
+    "pawn": {w: "./assets/styles/images/pieces/white-pawn.html",b:"./assets/styles/images/pieces/black-pawn.html",index:["a","b","c","d","e","f","g","h"]},
+    "bishop": {w: "./assets/styles/images/pieces/white-bishop.html",b:"./assets/styles/images/pieces/black-bishop.html",index:["c","f"]},
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,72 @@
+import {Pieces} from "./config/constants.js"
+
 const ChessBoardScreen = document.getElementsByClassName("board")[0];
 
-function InitialSetup() {
-    const list = ["a", "b", "c", "d", "e", "f", "g", "h"];
-    
-    for (let i = 0; i < 8; i++) {
-        const Row = document.createElement("ul");
-        Row.classList.add("f-row", i % 2 === 0 ? "f-row-even" : "f-row-odd");
-
-        for (let j = 0; j < 8; j++) {
-            const Index = document.createElement("li");
-            
-            
-            if (i === 7) {
-                const Node = document.createTextNode(`${list[j]}`);
-                Index.appendChild(Node);
-            }
-            
-            
-            if (j === 0) {
-                const Node = document.createTextNode(`${i + 1}`);
-                Index.appendChild(Node);
-            }
-            
-            Row.appendChild(Index);
-        }
-        
-        ChessBoardScreen.appendChild(Row);
-    }
+async function loadSVGPiece(filePath, targetElement) {
+  try {
+    const response = await fetch(filePath);
+    const svgText = await response.text();
+    targetElement.innerHTML = svgText;
+  } catch (error) {
+    console.error("Error loading SVG:", error);
+  }
 }
 
+function InitialSetup() {
+  const list = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+  for (let i = 0; i < 8; i++) {
+    const Row = document.createElement("ul");
+    Row.classList.add("f-row", i % 2 === 0 ? "f-row-even" : "f-row-odd");
+
+    for (let j = 0; j < 8; j++) {
+      const Index = document.createElement("li");
+      if (i === 7) {
+        const Node = document.createTextNode(`${list[j]}`);
+        Index.appendChild(Node);
+      }
+      if (j === 0) {
+        const Node = document.createTextNode(`${i + 1}`);
+        Index.appendChild(Node);
+      }
+
+      Row.appendChild(Index);
+    }
+
+    ChessBoardScreen.appendChild(Row);
+  }
+}
+
+function ArrangingPices() {
+  const allSquares = document.querySelectorAll(".board li");
+  const order = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]; 
+  
+  
+  for (let j = 0; j < 8; j++) {
+    let piece = order[j];
+    loadSVGPiece(Pieces[piece].w, allSquares[j]);
+  }
+  
+  
+  for (let i = 8; i <= 15; i++) {
+    loadSVGPiece(Pieces.pawn.w, allSquares[i]);
+  }
+    
+  
+  for (let i = 48; i < 56; i++) {
+    loadSVGPiece(Pieces.pawn.b, allSquares[i]);
+  }
+
+  
+  for (let j = 56; j < 64; j++) {
+    let piece = order[j - 56]; 
+    loadSVGPiece(Pieces[piece].b, allSquares[j]);
+  }
+}
+
+
+
+
 InitialSetup();
+ArrangingPices();
+
